4dab8a49593d91c1d6efdbfaa0c0065f40426491
leeolney <leeolney3@gmail.com>
2021-12-07 20:06:22 +0400

    add files

--- 2021/2021_50/2021_50.R
+++ 2021/2021_50/2021_50.R
@@ -63,6 +63,32 @@ spiders2 %>%
        fill="", color="", x="Year", y="Species count",
        caption="#TidyTuesday Week 50 | Data: World Spider Database")
 
+## updated plot: color change (thank you @bac3917 for the color contrast comment)
+spiders2 %>%
+  ggplot(aes(x=year, y=n, alluvium=country)) +
+  geom_alluvium(aes(fill=country, color=country),
+                alpha=.75, decreasing=FALSE) +
+  scale_fill_manual(values=c("#FAD510", "#CB2314", "#3B9AB2", "#354823")) +
+  scale_color_manual(values=c("#FAD510", "#CB2314", "#3B9AB2", "#354823")) +
+  scale_y_continuous(expand=c(0,0)) +
+  scale_x_continuous(expand=c(0.01,0.01)) +
+  cowplot::theme_minimal_hgrid(11.5) +
+  theme(text=element_text(family="Libre Franklin"),
+        legend.position = "top",
+        legend.margin=margin(l=-39),
+        plot.margin=margin(.75,1,.5,.75, unit="cm"),
+        plot.background = element_rect(fill="#f8f9fa",color=NA),
+        axis.title=element_text(face="bold", size=10),
+        axis.text=element_text(size=8.7),
+        plot.caption = element_text(margin=margin(t=8), size=8),
+        plot.title.position = "plot",
+        plot.title=element_text(size=12)
+        ) +
+  labs(title="Number of currently valid spider species described per year",
+       subtitle="by distribution region from 1980 to 2020, according to World Spider Database",
+       fill="", color="", x="Year", y="Species count",
+       caption="#TidyTuesday Week 50 | Data: World Spider Database")
+
 # gt 
 library(gt)
 library(gtExtras)
@@ -85,14 +111,17 @@ fg = spiders %>% group_by(family, genus) %>%
 range(fg$max_year)
 range(fg$min_year)
 
-## table
+## table 
+## updated big.mark="" (thank you @geokaramanis for pointing out separators)
 gt(fg) %>%
   gt_theme_538() %>%
   gt_plt_bar(column = n2, width=40, color="#606c38") %>%
   gt_sparkline(density, type="density", bw=.75, same_limit = TRUE, width = 30,) %>%
   gt_sparkline(histogram, type="histogram", bw=.75, same_limit = TRUE, width = 30,) %>%
-  gt_color_box(columns=min_year, domain=1757:1936, palette = "wesanderson::Zissou1") %>%
-  gt_color_box(columns=max_year, domain=2015:2021, palette = "wesanderson::Zissou1") %>%
+  gt_color_box(columns=min_year, domain=1757:1936, palette = "wesanderson::Zissou1",
+               big.mark = "") %>%
+  gt_color_box(columns=max_year, domain=2015:2021, palette = "wesanderson::Zissou1",
+               big.mark = "") %>%
   tab_spanner(
     label = "Year",
     columns = min_year:histogram
