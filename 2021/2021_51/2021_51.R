21a1ef1dff2f5a975de1380a8cbea9423eff555e
leeolney <leeolney3@gmail.com>
2021-12-14 11:57:42 +0400

    add files

--- 2021/2021_51/2021_51.R
+++ 2021/2021_51/2021_51.R
@@ -6,6 +6,12 @@ options(dplyr.summarise.inform = FALSE)
 library(ggtext)
 library(MetBrewer)
 
+library(showtext)
+font_add_google("Oswald", "oswald")
+font_add_google("Roboto Condensed", "roboto condensed")
+showtext_opts(dpi = 320)
+showtext_auto(enable = TRUE)
+
 # lyrics data 
 tuesdata <- tidytuesdayR::tt_load('2021-12-14')
 lyrics = tuesdata$lyrics
@@ -48,8 +54,22 @@ overall_df = artist_df %>%
 
 df1 = rbind(artist_df, overall_df)
 
-# theme
-theme1 = theme_minimal(base_family="roboto condensed", base_size = 13) +
+
+# tile plot
+p1 = df1 %>% 
+  filter(album_name!="Forever") %>%
+  mutate(album_name = case_when(album_name=="Spiceworld" ~ "**Spiceworld**<br>Album",
+                                album_name=="Spice" ~ "**Spice**<br>Album")
+         ) %>%
+  ggplot(aes(fct_relevel(track_number, "10", after = 9),
+             factor(str_to_title(artist), levels=c("All","Sporty","Scary","Posh","Ginger","Baby")),
+             fill=prop)) +
+  geom_tile(color="#e9ecef", size=.8) +
+  geom_text(aes(label=round(prop,1)), family="roboto condensed", size=3.6, color="white") +
+  scale_fill_gradientn(colors=met.brewer(name="Robert", 100, type = "continuous"),limits=c(0,100)) +
+  scale_x_discrete(position="top") +
+  facet_wrap(~album_name, ncol=1, strip.position = "left", scales="free_x") +
+  theme_minimal(base_family="roboto condensed", base_size = 13) +
   theme(strip.placement = "outside",
         strip.text.y.left=element_markdown(angle=0, lineheight = 1.4, size=11.5),
         panel.grid=element_blank(),
@@ -66,23 +86,7 @@ theme1 = theme_minimal(base_family="roboto condensed", base_size = 13) +
         plot.caption=element_text(size=9.1, color="grey20", margin=margin(t=14)),
         plot.background = element_rect(fill="#e9ecef", color=NA),
         legend.margin=margin(b=0)
-        ) 
-        
-# plot
-df1 %>% 
-  filter(album_name!="Forever") %>%
-  mutate(album_name = case_when(album_name=="Spiceworld" ~ "**Spiceworld**<br>Album",
-                                album_name=="Spice" ~ "**Spice**<br>Album")
-         ) %>%
-  ggplot(aes(fct_relevel(track_number, "10", after = 9),
-             factor(str_to_title(artist), levels=c("All","Sporty","Scary","Posh","Ginger","Baby")),
-             fill=prop)) +
-  geom_tile(color="#e9ecef", size=.8) +
-  geom_text(aes(label=round(prop,1)), family="roboto condensed", size=3.6, color="white") +
-  scale_fill_gradientn(colors=met.brewer(name="Robert", 100, type = "continuous"),limits=c(0,100)) +
-  scale_x_discrete(position="top") +
-  facet_wrap(~album_name, ncol=1, strip.position = "left", scales="free_x") +
-  theme1 +
+        ) +
   guides(fill = guide_colorbar(title.position = "top", 
                                 barwidth = unit(20, "lines"), 
                                 barheight = unit(.55, "lines"))) +
@@ -92,4 +96,65 @@ df1 %>%
        fill="Proportion of total lines by album track (%)")
        
 # save
-ggsave(p1, "2021_51.png", width=8, height=7, unit="in")
+ggsave("2021_51.png", width=8, height=7, unit="in")
+
+# Part 2: add track names
+track_df = tuesdata$studio_album_tracks %>% 
+  select(album_name, track_name, track_number) %>%
+  mutate(track_name= fct_inorder(track_name))
+  
+df2 = artist_df %>% 
+  filter(album_name!="Forever") %>%
+  mutate(track_number= as.numeric(track_number)) %>%
+  left_join(track_df, by=c("album_name","track_number"))  %>%
+  mutate(album_name = case_when(album_name=="Spiceworld" ~ "*Spiceworld Album (1997)*",
+                                album_name=="Spice" ~ "*Spice Album (1996)*"))
+
+# font                                
+font_add_google("Fira Sans Condensed")
+font_add_google("Fira Sans")
+f2 = "Fira Sans Condensed"
+f3 = "Fira Sans"    
+
+# dot plot
+p2 = df2 %>% 
+  ggplot(aes(x=str_to_title(artist), y=fct_rev(track_name), color=artist)) +
+  geom_line(aes(group=artist), show.legend = F) +
+  geom_point(aes(size=prop/100)) +
+  geom_text(data = df2 %>% filter(prop>50), aes(label=round(prop,1)), 
+            color="white", size=3, family=f2) +
+  facet_wrap(~album_name, ncol=2, scales="free_y") +
+  scale_y_discrete(labels = function(x) str_wrap(x, width = 18)) +
+  scale_color_manual(values=met.brewer(name="Veronese")) +
+  scale_x_discrete(position="top") +
+  scale_size_area(max_size = 13, labels=scales::percent) +
+  cowplot::theme_minimal_hgrid(10) +
+  guides(color="none") +
+  theme(strip.placement = "outside",
+        text=element_text(family=f2),
+        axis.line = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_text(margin=margin(r=5), size=9),
+        axis.text.x.top=element_text(size=10),
+        axis.text.y=element_text(margin=margin(r=0)),
+        strip.text.x.top=element_markdown(size=12, family=f3, color="grey40", margin=margin(b=3)),
+        plot.margin=margin(.5,.75,.5,.5, unit="cm"),
+        plot.title.position = "plot",
+        plot.title = element_markdown(size=14, family=f3, face="plain"),
+        legend.position = "top",
+        legend.margin=margin(l=-84, b=-2),
+        legend.title=element_text(size=10.5, family=f3),
+        legend.text=element_text(size=10.5,family=f3),
+        plot.caption=element_text(size=8.5,color="grey30"),
+        panel.grid.major.y = element_line(size=.4),
+        panel.spacing.x = unit(1.2, "lines")
+        )  +
+  labs(y="Track name (ascending album track order)",
+       title="**Spice Girls** lines in Spice and Spiceworld albums",
+       caption="\n#TidyTuesday Week 51 | Data from Spotify and Genius",
+       size="Proportion of total lines by album track")
+       
+# save
+ggsave("2021_50_p2.png", height=6, width=8, unit="in", bg="#fafafa")                          
+
