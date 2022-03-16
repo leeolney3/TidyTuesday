428b4b3a2ab6b09ff174671176107ad6d9576201
leeolney3 <leeolney3@gmail.com>
2022-03-16 19:40:47 +0400

    add files

--- 2022/week_11/2022_11.R
+++ 2022/week_11/2022_11.R
@@ -55,3 +55,74 @@ cran2 %>%
            
 # Save plot
 ggsave("2022_11.png", height=6.4, width=8, bg="white")
+
+# Table
+library(gt)
+library(gtExtras)
+
+# color palette packages
+selected = c("scico","paletteer","cartography","rcartocolor","wesanderson", 
+             "ggthemes","viridis","ggsci","ghibli","jcolors",
+             "pals", "nord","harrypotter","khroma","prism")
+
+cran2 = cran %>% filter(package %in% selected) %>%
+  mutate(date2=as.Date(date, "%Y-%m-%d")) %>%
+  mutate(date3 = case_when(is.na(date2) ~ mdy(paste(str_sub(date,5,11),substr(date, nchar(date)-4+1, nchar(date)))),
+                           TRUE~date2
+  )) %>%
+  select(-date2) 
+
+# prepare data
+df1 = cran2 %>% 
+  group_by(package) %>%
+  filter(date3==max(date3)) %>%
+  select(package, c_version=version, c_date=date3)
+
+df2 = cran2 %>%
+  count(package, date3) %>%
+  complete(date3, package,fill = list(n = 0)) %>%
+  group_by(package) %>%
+  summarise(timeline = list(n))
+
+df3 = cran2 %>% group_by(package) %>%
+  summarise(,release_year=year(min(date3)),total_releases=n()) %>%
+  left_join(df1, by="package") %>%
+  left_join(df2, by="package") %>% 
+  relocate(c(c_version, c_date), .after=timeline)
+
+# gt table
+df3 %>% 
+  arrange(release_year) %>%
+  gt() %>%
+  gt_theme_538() %>%
+  gtExtras::gt_sparkline(timeline, label=FALSE,
+                         range_colors=c("#ced4da","#ef233c"),
+                         line_color="#ced4da",
+                         width = 40
+  ) %>%
+  cols_label(total_releases=md("Total<br>releases"),
+             release_year=md("Release year"),
+             c_version = md("Number"),
+             c_date=md("Date")) %>%
+  tab_spanner(columns=c("c_version", "c_date"), label="Current version") %>%
+  tab_footnote(footnote=md("Current version listed in dataset"),
+               location=cells_column_spanners("Current version")) %>%
+  opt_table_font(
+    font = list(
+      google_font("Outfit"),
+      default_fonts()
+    ),
+    weight = 300
+  ) %>%
+  tab_options(footnotes.font.size = 12,
+              heading.subtitle.font.size=13.5,
+              table.font.size = 14.5) %>%
+  tab_style(style = list(cell_text(weight = 450)),
+            locations = cells_body(columns = package)) %>%
+  tab_source_note(source_note = "#TidyTuesday Week 11  |  Data from Robert Flight GitHub | Sparkline inspired by Benjamin Nowak @BjnNowak") %>%
+  tab_header(title="CRAN packages releases",
+             subtitle="15 packages containing color palettes, arranged in chronological order of release year.") %>%
+  gt_color_box(columns=total_releases, domain=range(df3$total_releases),
+               palette="ggsci::indigo_material", width=50) %>%
+  cols_align(align="center",columns = c(release_year, total_releases)) 
+
