5316e337fcc54f57065f1ad80d164a17add5b51e
leeolney3 <leeolney3@gmail.com>
2022-03-29 10:11:49 +0400

    add files

--- 2022/week_13/2022_13.R
+++ 2022/week_13/2022_13.R
@@ -14,6 +14,7 @@ showtext_auto(enable = TRUE)
 
 sports <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-03-29/sports.csv')
 
+## Profitability of college football and other sports
 # Prepare data
 df = sports %>%
   filter(!str_detect(sports, 'Track and Field') )  %>%
@@ -61,3 +62,52 @@ df %>%
 
 # Save plot       
 ggsave("2022_13.png", bg="white")
+
+
+## Add average point and annotation
+df_lab = df %>% group_by(col, year) %>%
+  summarise(profit = mean(profit, na.rm = T)) %>%
+  ungroup() %>%
+  mutate(mean_profit=case_when(year==2015 & col=="Football"~paste("Average:",scales::number(profit, accuracy=1, big.mark = ",", prefix="$")),TRUE~paste(scales::number(profit, accuracy=1, big.mark = ",", prefix="$"))))
+  
+df %>%
+  ggplot(aes(fct_rev(col), profit)) +
+  stat_interval(.width=c(.95,1), size=3, alpha=.8) +
+  scale_color_manual(name="Level:",values=c("#B0B6BA","black"),
+                     labels = function(x) paste0(as.numeric(x)*100, "%")) +
+  ggnewscale::new_scale_color() +
+  geom_point(aes(color=col),shape=21, size=2, position=position_nudge(x=-0.3, y=0), show.legend = F) +
+  scale_color_manual(name="Group:",values=alpha(c("#176746","#7c5eae"),.8)) +
+  stat_summary(geom="point", fun=mean, shape=18, size=3,position=position_nudge(x=-0.3, y=0), color="#ffff3f") +
+  geom_text(data=df_lab, aes(label=mean_profit, color=col), show.legend = F,
+            size=2.5, family="Outfit", position=position_nudge(x=-0.6, y=0), alpha=1) +
+  scale_y_continuous(labels=scales::label_number(suffix="M", scale=1e-6), expand=c(0.02,0.02)) +
+  scale_x_discrete(expand = expansion(mult = c(.8,0))) +
+  coord_flip() +
+  facet_wrap(~year, ncol=1, strip.position="left") +
+  theme_minimal() +
+  theme(text=element_text(family="Outfit"),
+        panel.grid=element_line(size=.3),
+        strip.placement = "outside",
+        strip.text.y.left = element_text(angle=0, color="black", size=10),
+        legend.position = "top",
+        axis.title.y=element_blank(),
+        axis.title.x=element_text(size=10),
+        plot.margin=margin(.5,.5,.5,.5, unit="cm"),
+        legend.justification = "left",
+        legend.title=element_text(size=9),
+        legend.text=element_text(size=9),
+        legend.margin=margin(l=-88, b=-7),
+        plot.caption.position="plot",
+        plot.caption=element_text(hjust=0, size=8.3, color="grey20"),
+        plot.title.position="plot",
+        plot.title=element_markdown(size=15),
+        plot.subtitle = element_text(size=9),
+        panel.grid.major.y=element_blank()
+        ) +
+  labs(y="Profit (in USD)",
+       caption="\n#TidyTuesday week 13 | Data from Equity in Athletics Data Analysis",
+       title="Profitability of college <span style='color:#176746'>football</span> and <span style='color:#7c5eae'>other sports</span>",
+       subtitle="From 2015 to 2019, each point represents one record of every sport per institution")
+       
+ggsave("2022_13b.png", bg="white")
