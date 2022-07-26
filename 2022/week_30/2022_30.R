1fa4c6ae7043c46e07c13ba9036c72d59e38cd94
Lee Olney <leeolney3@gmail.com>
2022-07-26 21:25:52 +0400

    add files

--- 2022/week_30/2022_30.R
+++ 2022/week_30/2022_30.R
@@ -12,52 +12,55 @@ showtext_auto(enable = TRUE)
 # Load fonts
 font_add_google("Barlow")
 f1 = "Barlow"
+font_add_google("Lato")
+f2 = "Lato"
 
-# 2022 Austrian Grand Prix data
-#code from: Meghan Hall @MeghanMHall https://twitter.com/MeghanMHall/status/1549183367769366537
-drivers <- load_drivers(season = 2022) %>% 
-  filter(code != "HUL")
+# 2022 round 10: FORMULA 1 LENOVO BRITISH GRAND PRIX 2022 
+# https://www.formula1.com/en/results.html/2022/races/1114/great-britain/fastest-laps.html
+drivers = load_drivers(season = 2022) %>% filter(code %in% c("HAM","SAI","PER","LEC","ALO","NOR","MSC","VER","STR","VET","RIC","MAG","LAT","OCO","TSU","GAS","BOT")) 
+
+# function adapted from Meghan Hall @MeghanMHall https://twitter.com/MeghanMHall/status/1549183367769366537
 fastestlaps <- function(dr) {
-  get_driver_telemetry(2022, 11, driver = dr, fastest_only = TRUE)
+  get_driver_telemetry(2022, race = 10,  session="R",driver=dr,fastest_only = TRUE)
 }
-austria <- purrr::map_df(unique(drivers$code), fastestlaps)
+r10 <- purrr::map_df(unique(drivers$code), fastestlaps)
 
 # Driver name and lap times
-x = load_laps(season = "current", 11)
-x2 = x %>% group_by(driverId) %>% filter(time==min(time)) %>%
-  arrange(time_sec) %>%
-  left_join(drivers, by="driverId") %>%
+x2 = load_laps(season = "current", 10) %>%
+  group_by(driverId) %>% filter(time==min(time)) %>%
+  arrange(time) %>%
+  left_join(drivers,by="driverId") %>%
   mutate(name_long=glue::glue("{givenName} {familyName}"))%>%
-  mutate(lab=glue::glue("{name_long}<br><span style = 'font-size:8pt'>{time_sec} sec</span>"),
+  mutate(lab=glue::glue("{name_long}<br><span style = 'font-size:7.5pt'>{time}</span>"),
          lab=fct_inorder(lab)) %>%
   select(code, lab)
   
 # Plot
-austria %>% left_join(x2, by=c("driverCode"="code")) %>%
+r10 %>% left_join(x2, by=c("driverCode"="code")) %>%
   ggplot(aes(X,Y, group=lab, color=factor(nGear))) +
   geom_path(size=1.8) +
   scale_color_viridis_d("Gear:",option="turbo") +
   scale_fill_viridis_d("Gear:",option="turbo") +
-  facet_wrap(~factor(lab, levels=x2$lab), ncol=4) +
+  facet_wrap(~factor(lab, levels=x2$lab), ncol=6) +
   coord_fixed() +
   theme_void() +
-  theme(text=element_text(family=f1),
+  theme(text=element_text(family=f2),
         legend.position = "top",
         legend.justification = "left",
-        legend.title=element_text(size=10),
-        legend.text=element_text(size=9.5),
-        legend.margin=margin(b=5),
+        legend.title=element_text(size=9.5),
+        legend.text=element_text(size=9),
+        legend.margin=margin(b=5, t=7),
         plot.margin=margin(.4,.4,.3,.4,unit="cm"),
         plot.background = element_rect(fill="grey98", color=NA),
-        plot.subtitle=element_text(size=10),
-        plot.title=element_text(size=13, face="bold"),
-        plot.caption = element_text(color="grey30",size=8,margin=margin(t=12)),
+        plot.subtitle=element_text(size=9.5),
+        plot.title=element_text(size=12, face="bold"),
+        plot.caption = element_text(color="grey30",size=7.7,margin=margin(t=12)),
         panel.spacing = unit(.9, "lines"),
-        strip.text=element_markdown(size=9),
+        strip.text=element_markdown(size=8.4, family=f1),
         ) +
   guides(color=guide_legend(nrow=1)) +
-  labs(caption="#TidyTuesday week 30 BYOD •  Source: {f1dataR} package",
-       title="2022 Austrian Grand Prix",
-       subtitle="Fastest lap by driver") 
+  labs(caption="#TidyTuesday week 30  •  Source: {f1dataR} package",
+       title="FORMULA 1 LENOVO BRITISH GRAND PRIX 2022",
+       subtitle="Fastest lap by driver, arranged in ascending order of fastest lap time")
        
-ggsave("2022_30.png", height=7.5, width=6.5)        
+ggsave("2022_30.png", height=7, width=6.8)        
