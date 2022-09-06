7cbde6dd37a7fafb645ce26631cdb4938da3d7cd
Lee Olney <leeolney3@gmail.com>
2022-09-06 08:33:04 +0400

    update 2022_36

--- 2022/week_36/2022_36.R
+++ 2022/week_36/2022_36.R
@@ -1,11 +1,11 @@
 # 2022-09-06
 # TidyTuesday week 26 LEGO database, rebrickable courtesy of Georgios Karamanis.
-# Lego color plots, inspired by [Edurne Morillo](https://blog.datawrapper.de/lego-sets-colors-history/)
+
 
 # Load libraries
 library(tidyverse)
-library(ggalluvial)
-library(patchwork)
+library(ggtext)
+library(ggimage)
 library(showtext)
 showtext_opts(dpi = 300)
 showtext_auto(enable = TRUE)
@@ -21,96 +21,78 @@ sets <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidyt
 inventory_parts = readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-09-06/inventory_parts.csv.gz")
 colors = readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-09-06/colors.csv.gz")
 
-# Wrangle
+# Merge data
 df= inventories %>% left_join(inventory_parts, by=c("id"="inventory_id")) %>%
   left_join(colors, by=c("color_id"="id")) %>%
   filter(is_spare==FALSE) %>%
   left_join(sets %>% select(-num_parts,-img_url), by="set_num") %>%
-  filter(between(year, 1990,2021))
-  
-## soild bricks colors
-selected_colors1= df %>% filter(is_trans==FALSE, color_id!=9999) %>%
-  group_by(year,color_id, name.x, rgb, is_trans) %>%
-  tally(quantity) %>%
-  ungroup() %>%
-  group_by(color_id) %>%
-  summarise(n_year=n_distinct(year)) %>%
-  arrange(desc(n_year)) %>%
-  filter(n_year==max(n_year)) %>%
-  pull(color_id)
-df1 = df %>% 
-  group_by(year,color_id, name.x, rgb, is_trans) %>%
-  tally(quantity) %>%
-  ungroup() %>%
-  group_by(year) %>%
-  mutate(pct=n/sum(n)) %>%
-  ungroup() %>%
-  filter(color_id %in% color_id) %>%
-  mutate(col=glue::glue("#{rgb}")) %>%
-  filter(color_id %in% selected_colors1) 
-df1c= df1 %>% select(name.x, col) %>%
-  distinct() %>% arrange(name.x)
-  
-## translucent
-selected_colors2= df %>% filter(is_trans==TRUE, color_id!=9999) %>%
-  group_by(year,color_id, name.x, rgb, is_trans) %>%
-  tally(quantity) %>%
-  ungroup() %>%
-  group_by(color_id) %>%
-  summarise(n_year=n_distinct(year)) %>%
-  arrange(desc(n_year)) %>%
-  filter(n_year==max(n_year)) %>%
-  pull(color_id)
-df2 = df %>% 
-  group_by(year,color_id, name.x, rgb, is_trans) %>%
-  tally(quantity) %>%
-  ungroup() %>%
-  group_by(year) %>%
-  mutate(pct=n/sum(n)) %>%
-  ungroup() %>%
-  filter(color_id %in% color_id) %>%
-  mutate(col=glue::glue("#{rgb}")) %>%
-  filter(color_id %in% selected_colors2) 
-df2c= df2 %>% select(name.x, col) %>%
-  distinct() %>% arrange(name.x)  
-  
-# Plot
-theme1= cowplot::theme_minimal_grid(10) +
-  theme(text=element_text(family=f1),
-        panel.grid=element_blank(),
-        axis.ticks.length=unit(.2, "cm"),
-        axis.ticks=element_line(color="black", size=.25),
-        plot.background = element_rect(fill="#BEC1C3",color=NA),
-        plot.title.position = "plot",
-        plot.title=element_text(size=13, face="bold.italic"),
-        plot.margin=margin(t=12))
-p1 = df1 %>% ggplot(aes(x=year, y=pct, alluvium=name.x)) +
-  ggalluvial::geom_alluvium(aes(fill=name.x),decreasing = FALSE, alpha=1, show.legend = FALSE) +
-  scale_fill_manual(values=df1c$col) +
-  scale_y_continuous("Percentage", labels=scales::percent, expand=c(0.01,0.01), breaks=seq(0,.8,.1)) +
-  scale_x_continuous("Year", breaks=seq(1990,2020,5),expand=c(0.02,0.02)) +
-  theme1 +
-  labs(title="Soild Colors")
+  filter(is_trans==FALSE, color_id!=9999,between(year, 2002,2021))
 
-p2 = df2 %>% ggplot(aes(x=year, y=pct, alluvium=name.x)) +
-  ggalluvial::geom_alluvium(aes(fill=name.x, color=name.x),decreasing = FALSE, alpha=.7, show.legend = FALSE, size=.7) +
-  scale_fill_manual(values=df2c$col) +
-  scale_color_manual(values=df2c$col) +
-  scale_y_continuous("Percentage", labels=scales::percent, expand=c(0.001,0.001)) +
-  scale_x_continuous("Year", breaks=seq(1990,2020,5),expand=c(0.02,0.02)) +
-  theme1 +
-  labs(title="Translucent colors")
+# Most common soild colors from 2002 to 2021
+s1= df %>% 
+  group_by(year,color_id, name.x, rgb, is_trans) %>%
+  tally(quantity) %>%
+  ungroup() %>%
+  group_by(name.x) %>%
+  summarise(n_year=n_distinct(year)) %>%
+  filter(n_year==20) 
   
-p1/p2 +
-    plot_annotation(title = 'Most common LEGO colors',
-                    subtitle= 'Percentage of color by set year, from 1990 to 2021',
-                    caption='TidyTuesday week 35  •  Data from rebrickable',
-                    theme = 
-                      theme(plot.title = element_text(hjust=.5, size = 16, family=f1, face="bold"),
-                      plot.subtitle=element_text(size=9,hjust=.5,family=f1, margin=margin(b=-5)),
-                      plot.caption=element_text(size=8.5,family=f1,margin=margin(t=10), color="grey20"),
-                      plot.margin=margin(.5,.5,.4,.5,unit="cm"),
-                      plot.background = element_rect(fill="#BEC1C3",color=NA))
-                    )    
+s2= df %>% filter(name.x %in% s1$name.x) %>%
+  group_by(name.x) %>%
+  tally(quantity, sort=TRUE)%>%
+  filter(n>100000) #7 colors with more than 100000 bricks
   
-ggsave("2022_36.png", height=9, width=6.6)
+# Wrangle
+d1= df %>% 
+  group_by(year, color_id, name.x, rgb, is_trans) %>%
+  tally(quantity) %>%
+  mutate(grp=case_when(!name.x %in% s2$name.x~"Other", TRUE~paste(name.x))) %>%
+  ungroup() %>%
+  group_by(year,grp) %>%
+  tally(n) %>%
+  mutate(prop=round(n/sum(n)*100)) %>%
+  select(-n) %>%
+  pivot_wider(names_from = grp, values_from =prop) %>%
+  select(-Other) %>%
+  mutate(Other=100-Black-Blue-`Light Bluish Gray`-Red-Tan-White-Yellow) %>%
+  ungroup() %>%
+  pivot_longer(!year) 
+
+d2= d1 %>% uncount(value) %>%
+  mutate(name=factor(name, levels=s2$name.x)) %>%
+  group_by(year) %>%
+  arrange(name, .by_group = TRUE) %>%
+  mutate(x=rep(1:10,10),
+         id=row_number(),
+         y=cut(id, breaks = seq(0,100,10), label=seq(1,10,1))) %>%
+  select(-id) %>%
+  ungroup() %>%
+  mutate(image=case_when(name=="Black"~"bricks/Black.png",
+                       name=="Blue"~"bricks/Blue.png",
+                       name=="Tan"~"bricks/Tan.png",
+                       name=="Red"~"bricks/Red.png",
+                       name=="White"~"bricks/White.png",
+                       name=="Yellow"~"bricks/Yellow.png",
+                       name=="Light Bluish Gray"~"bricks/Light.png",
+                       ))
+                       
+# Waffle plot
+# inspired by Abdoul Madjid @issa_madjid https://twitter.com/issa_madjid/status/1526685250994286592/photo/1
+d2 %>% drop_na(image) %>%
+  ggplot(aes(x=x, y=y)) +
+  geom_image(aes(image=image), size=.1) +
+  facet_wrap(~year, ncol=5, strip.position = "bottom") +
+  cowplot::theme_map(10) +
+  theme(text=element_text(family=f1),
+        panel.spacing.y = unit(.6, "lines"),
+        panel.spacing.x = unit(.8, "lines"),
+        strip.text=element_text(hjust=0, face="bold", color="grey50", size=8.9),
+        plot.title=element_text(face="bold.italic", size=14),
+        plot.subtitle=element_markdown(lineheight=1.2, size=8.8),
+        plot.margin=margin(.5,.5,.4,.5,unit="cm"),
+        plot.caption=element_text(margin=margin(t=10))) +
+  labs(title=str_to_upper("Lego Colors"),
+       subtitle="**One brick represents 1% of bricks by set year** from 2022 to 2021, including seven most common soild lego colors <br>(Black, Light Bluish Gray, White, Red, Blue, Tan, and Yellow).",
+       caption="TidyTuesday week 35  •  Data from rebrickable.com")
+       
+ggsave("2020_36.png", height=7, width=7, bg="white")                                
